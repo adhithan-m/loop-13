aa7be3581fca45a9b793a66e7cb9c45af48cce5a
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 19:47:50 +0530

    age by month.

---
+++ age-month/js/age-month.js
@@ -0,0 +1,27 @@
+//#age finding using months
+
+// n=prompt("enter the date mm-dd-yyyy")
+		  // v= new Date()
+		  // currYear=v.getFullYear()
+		  // currmonth=v.getMonth()
+		  
+		 
+		  // const bY = new Date(n);
+		  // const year = bY.getFullYear();
+		  
+		  // bm=bY.getMonth();
+		  // currDate=v.getDate();
+		  // birthdate=bY.getDate();
+		  
+		  // age=(currYear-year)
+		  // if(bm>=currmonth && currDate>=birthdate)
+		  // {
+	
+			
+		  // document.write(`your age is : {age}`)
+		  // }
+		  // else
+		  // {
+		  // document.write(`your age is : {age-1}`)	
+		  // }
+		  
